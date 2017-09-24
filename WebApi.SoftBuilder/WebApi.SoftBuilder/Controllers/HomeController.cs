ad5f2b35be9b433bf0d0cc2ea02d94aee4041b96
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-09-24 22:12:16 +0530

    Removing unnecessary data

--- WebApi.SoftBuilder/WebApi.SoftBuilder/Controllers/HomeController.cs
+++ WebApi.SoftBuilder/WebApi.SoftBuilder/Controllers/HomeController.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Http;
 
 namespace WebApi.SoftBuilder.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
-        public ActionResult Index()
-        {
-            ViewBag.Title = "Home Page";
-
-            return View();
-        }
+        
     }
 }
