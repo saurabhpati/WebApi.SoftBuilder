524c53decac1e2cb9833ff6bdfd50398475d3d6a
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-10-02 19:28:46 +0530

    Implementing a factory pattern.

---
+++ WebApi.SoftBuilder/WebApi.SoftBuilder.Implementation/WebApi.SoftBuilder.Implementation/Factory/ServiceFactory/HomePageDataServiceFactory.cs
@@ -0,0 +1,10 @@
+﻿using WebApi.SoftBuilder.Implementation.Model;
+using WebApi.SoftBuilder.Implementation.Service;
+using WebApi.SoftBuilder.Shared.Factory.ServiceFactory;
+
+namespace WebApi.SoftBuilder.Implementation.Factory.ServiceFactory
+{
+    public class HomePageDataServiceFactory<T, U> : IHomePageDataServiceFactory<T, U> where T : HomePageDataServiceBase<U>, new() where U : ClientModelBase
+    {
+    }
+}
---
+++ WebApi.SoftBuilder/WebApi.SoftBuilder.Shared/WebApi.SoftBuilder.Shared/Factory/ServiceFactory/IHomePageDataServiceFactory.cs
@@ -0,0 +1,9 @@
+﻿using WebApi.SoftBuilder.Shared.Model;
+using WebApi.SoftBuilder.Shared.Service;
+
+namespace WebApi.SoftBuilder.Shared.Factory.ServiceFactory
+{
+    public interface IHomePageDataServiceFactory<T, U> : IPageDataServiceFactory<T, U> where T : IHomePageDataService<U> where U : IClientModel
+    {
+    }
+}
