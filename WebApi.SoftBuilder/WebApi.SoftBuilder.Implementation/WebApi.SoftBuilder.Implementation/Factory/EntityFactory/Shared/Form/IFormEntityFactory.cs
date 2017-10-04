ddd71c679fc573ff538266cfa24d8995717fe299
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-10-04 22:41:04 +0530

    Form Entity Factory

--- WebApi.SoftBuilder/WebApi.SoftBuilder.Implementation/WebApi.SoftBuilder.Implementation/Factory/EntityFactory/Shared/Form/IFormEntityFactory.cs
+++ WebApi.SoftBuilder/WebApi.SoftBuilder.Implementation/WebApi.SoftBuilder.Implementation/Factory/EntityFactory/Shared/Form/IFormEntityFactory.cs
@@ -1,13 +1,9 @@
 ﻿using WebApi.SoftBuilder.Implementation.Entity.Shared.Form;
-using WebApi.SoftBuilder.Implementation.Model;
 using WebApi.SoftBuilder.Shared.Factory.ModelFactory;
 
 namespace WebApi.SoftBuilder.Implementation.Factory.EntityFactory.Shared.Form
 {
     public interface IFormEntityFactory<T> : IEntityFactory<T> where T : IFormEntity
     {
-        T GetFieldEntity();
-
-        T GetMetaEntity();
     }
 }
