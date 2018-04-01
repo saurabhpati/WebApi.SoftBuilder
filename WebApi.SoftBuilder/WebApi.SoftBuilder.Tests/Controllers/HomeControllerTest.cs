eea1ab5a54fb97a33b918daee2f2fca3df58d2af
Saurabh Pati <saurabhpati.pati@gmail.com>
2018-04-01 20:41:03 +0530

    About action test

--- WebApi.SoftBuilder/WebApi.SoftBuilder.Tests/Controllers/HomeControllerTest.cs
+++ WebApi.SoftBuilder/WebApi.SoftBuilder.Tests/Controllers/HomeControllerTest.cs
@@ -1,12 +1,52 @@
-﻿using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebApi.SoftBuilder;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebApi.SoftBuilder.Controllers;
+using WebApi.SoftBuilder.Implementation.Entity.Home;
+using WebApi.SoftBuilder.Implementation.Entity.Shared;
+using WebApi.SoftBuilder.Implementation.Factory.EntityFactory;
+using WebApi.SoftBuilder.Implementation.Factory.EntityFactory.Home;
+using WebApi.SoftBuilder.Implementation.Factory.EntityFactory.Shared.Form;
+using WebApi.SoftBuilder.Implementation.Factory.ServiceFactory;
+using WebApi.SoftBuilder.Implementation.Service;
+using WebApi.SoftBuilder.Shared.Factory.ModelFactory;
+using WebApi.SoftBuilder.Shared.Factory.ServiceFactory;
+using WebApi.SoftBuilder.Shared.Model;
+using WebApi.SoftBuilder.Shared.Service;
 
 namespace WebApi.SoftBuilder.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
+        private static IHomePageDataService<HomeEntity> homePageDataService;
+        private static IEntityFactory<DisplayDataEntity> displayDataEntityFactory;
+        private static IHomeEntityFactory homeEntityFactory;
+        private static IFormEntityFactory formEntityFactory;
+        private static IHomePageDataServiceFactory<HomePageDataService, HomeEntity> homePageDataServiceFactory;
+        private static HomeController homeController;
+
+        [ClassInitialize]
+        public static void InitHomeControllerTest(TestContext testContext)
+        {
+            formEntityFactory = new FormEntityFactory();
+            homeEntityFactory = new HomeEntityFactory();
+            displayDataEntityFactory = new DisplayDataEntityFactory();
+            homePageDataServiceFactory = new HomePageDataServiceFactory(displayDataEntityFactory, homeEntityFactory, formEntityFactory);
+            homePageDataService = new HomePageDataService(displayDataEntityFactory, homeEntityFactory, formEntityFactory);
+            homeController = new HomeController(homePageDataServiceFactory);
+        }
+
+        [TestMethod]
+        public void AboutTest()
+        {
+            AboutEntity entity = homeController.About();
+
+            Assert.IsNotNull(entity);
+            Assert.IsNotNull(entity.Id);
+            Assert.IsNotNull(entity.Name);
+            Assert.IsNotNull(entity.DisplayDataList);
+            Assert.Equals(entity.Id, "about");
+            Assert.Equals(entity.Name, "About");
+            Assert.Equals(entity.DisplayDataList.Count, 2);
+        }
     }
 }
